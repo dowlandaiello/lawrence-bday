de7b6ea1959c46446ebac0c5adbd9f22bef768ca
Dowland Aiello <dowlandaiello@gmail.com>
2022-05-22 21:01:08 -0700

    More gitignore stuff
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use yew::{Component, Context, Html};
+
+/// The root. No props needed.
+struct App;
+
+impl Component for App {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app<App>();
 }
